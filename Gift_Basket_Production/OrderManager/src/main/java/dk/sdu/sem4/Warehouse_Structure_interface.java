56ac5500d3db1b9ac9fd7592f1f29145ea8fd2dc
Fard121 <fjama23@student.sdu.dk>
2026-04-23 13:34:51 +0200

    Warehouse structure class and interface

---
+++ Gift_Basket_Production/OrderManager/src/main/java/dk/sdu/sem4/Warehouse_Structure_interface.java
@@ -0,0 +1,34 @@
+package dk.sdu.sem4;
+
+public interface Warehouse_Structure_interface {
+
+
+    public boolean assign_Warehouse_component(Warehouse_Component_Interface Component);
+
+    public int get_Warehouse_Process();
+
+    public int get_Warehouse_Process_Status();
+
+    public boolean insert_Order(Order_Class order);
+
+    public boolean insert_Item(int order_ID, Order_Item_Class & item, int amount = 1);
+
+    public order_Class extract_Order();
+
+    public boolean extract_Item(int order_ID, Order_Item_Class & item, int amount = 1);
+
+    public String get_InventoryList();
+
+    public boolean check_Inventory_forOrder(int order_ID);
+
+    public int check_Inventory_forItem(Item_Class item);
+
+    public boolean check_CurrentlyLoaded();
+
+
+
+
+
+
+
+}
