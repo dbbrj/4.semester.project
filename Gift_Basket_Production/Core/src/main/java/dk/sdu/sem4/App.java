2359fb0756d398eb2ba3f08fbfb6146522d79e9d
Ditte Høgedal <dbbrj@hotmail.com>
2026-04-26 16:37:40 +0200

    updated the docker-compose to include mqtt

--- Gift_Basket_Production/Core/src/main/java/dk/sdu/sem4/App.java
+++ Gift_Basket_Production/Core/src/main/java/dk/sdu/sem4/App.java
@@ -1,10 +1,19 @@
 package dk.sdu.sem4;
 
-/**
- * Hello world!
- */
+import dk.sdu.sem4.assembly.AssemblyStationController;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        String brokerUrl = "tcp://mqtt:1883";  // "tcp://localhost:1883" for local testing
+
+        AssemblyStationController assemblyController = new AssemblyStationController(brokerUrl);
+
+        boolean connected = assemblyController.connect();
+        if (connected) {
+            System.out.println("Assembly status: " + assemblyController.checkStatus());
+        } else {
+            System.out.println("Failed to connect to assembly station!");
+        }
     }
 }
--- Gift_Basket_Production/Core/src/main/java/module-info.java
+++ Gift_Basket_Production/Core/src/main/java/module-info.java
@@ -1,3 +1,4 @@
 module core {
+    requires assembly_struct;
     
 }
