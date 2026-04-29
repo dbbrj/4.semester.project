fe97efae6d05ba3eb1c906a34240c9d03baeb8ab
Ditte Jensen <dbbrj@hotmail.com>
2026-04-29 14:26:40 +0200

    removed the test code

--- Gift_Basket_Production/Core/src/main/java/dk/sdu/sem4/App.java
+++ Gift_Basket_Production/Core/src/main/java/dk/sdu/sem4/App.java
@@ -1,32 +1,6 @@
 package dk.sdu.sem4;
 
-import dk.sdu.sem4.assembly.AssemblyStationController;
-import dk.sdu.sem4.config.Config_Machine_Orchestrator;
-import dk.sdu.sem4.config.Config_file_reader;
-
 public class App {
     public static void main(String[] args) {
 
-        String brokerUrl = "tcp://mqtt:1883";  // "tcp://localhost:1883" for local testing
-
-        AssemblyStationController assemblyController = new AssemblyStationController(brokerUrl);
-
-        boolean connected = assemblyController.connect();
-        if (connected) {
-            System.out.println("Assembly status: " + assemblyController.checkStatus());
-        } else {
-            System.out.println("Failed to connect to assembly station!");
-        }
-        
-        Config_file_reader reader = new Config_file_reader();
-
-        if (reader.load_Config_file()) {
-            Config_Machine_Orchestrator config = reader.getConfig_machine_orchestrator();
-            System.out.println("Machine: " + config.getMachineName());
-            System.out.println("IP: " + config.getIpAddress());
-            System.out.println("Port: " + config.getPort());
-        } else {
-            System.out.println("Failed to load config!");
-        }
-    }
 }
--- Gift_Basket_Production/Core/src/main/java/module-info.java
+++ Gift_Basket_Production/Core/src/main/java/module-info.java
@@ -1,6 +1,3 @@
 module core {
-    requires assembly_struct;
-    requires config_file_reader;
-        
-    requires machine_orch;
+
 }
