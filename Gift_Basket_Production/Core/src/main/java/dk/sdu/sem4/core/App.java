c3aca2ddb9b81be3c54745572e37fdee23e42718
Ditte Jensen <dbbrj@hotmail.com>
2026-04-29 15:35:08 +0200

    Remove config loading from App class
    Removed configuration loading logic from main method.

--- Gift_Basket_Production/Core/src/main/java/dk/sdu/sem4/core/App.java
+++ Gift_Basket_Production/Core/src/main/java/dk/sdu/sem4/core/App.java
@@ -1,23 +1,10 @@
 package dk.sdu.sem4.core;
 
-import dk.sdu.sem4.config.Config_Machine_Orchestrator;
-import dk.sdu.sem4.config.Config_file_reader;
-
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
 
-        Config_file_reader reader = new Config_file_reader();
-
-        if (reader.load_Config_file()) {
-            Config_Machine_Orchestrator config = reader.getConfig_machine_orchestrator();
-            System.out.println("Machine: " + config.getMachineName());
-            System.out.println("IP: " + config.getIpAddress());
-            System.out.println("Port: " + config.getPort());
-        } else {
-            System.out.println("Failed to load config!");
-        }
-    }
+        
 }
