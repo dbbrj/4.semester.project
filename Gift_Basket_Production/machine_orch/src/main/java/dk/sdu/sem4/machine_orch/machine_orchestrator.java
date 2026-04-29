64c6c68388b696998ef92939e9792537c7bb3f69
Ditte Jensen <dbbrj@hotmail.com>
2026-04-29 14:18:03 +0200

    removed the content of the modules that has been moved to core

--- Gift_Basket_Production/machine_orch/src/main/java/dk/sdu/sem4/machine_orch/machine_orchestrator.java
+++
@@ -1,7 +0,0 @@
-package dk.sdu.sem4.machine_orch;
-
-public class machine_orchestrator {
-    public static void machineOrchestrator() {
-        System.out.println("Machine Orchestrator is running...");
-    }
-}
